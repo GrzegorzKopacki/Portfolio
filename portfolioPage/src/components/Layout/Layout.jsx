9197bd425859c99ea018e420c6edb3a21c63b684
Grzegorz <gkopacki92@gmail.com>
2024-05-05 23:47:08 +0200

    created maincontent component

---
+++ portfolioPage/src/components/Layout/Layout.jsx
@@ -0,0 +1,11 @@
+import { MainContent } from "../MainContent/MainContent";
+
+export function Layout() {
+	return (
+		<>
+			<MainContent>
+                <div>HELLO WORLD</div>
+            </MainContent>
+		</>
+	);
+}
--- portfolioPage/src/main.jsx
+++ portfolioPage/src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Layout } from "./components/Layout/Layout";
 
 
 const router = createBrowserRouter([
