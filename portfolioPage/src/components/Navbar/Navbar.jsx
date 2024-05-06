349bac3bcf79b8df0bc0288a6fa217c7be903474
Grzegorz <gkopacki92@gmail.com>
2024-05-07 00:04:26 +0200

    styled ul tag in navbar component

--- portfolioPage/src/components/Navbar/Navbar.jsx
+++ portfolioPage/src/components/Navbar/Navbar.jsx
@@ -1,18 +1,18 @@
 import { LANGUAGE } from "../../constants/language";
 import { BurgerButton } from "../BurgerButton/BurgerButton";
 import { SwitchMenu } from "../SwitchMenu/SwitchMenu";
+import styles from "./Navbar.module.css";
 
 export function Navbar({ language }) {
 	const selectedLanguage = LANGUAGE.find((lang) => lang.language === language);
 
 	return (
-		<div>
+		<div className={styles.header}>
 			<SwitchMenu />
-
-			<ul>
+			<ul className={styles.menu}>
 				{selectedLanguage &&
 					selectedLanguage.navbarItems.map((item, index) => (
-						<li key={index}>
+						<li className={styles.menuLink} key={index}>
 							<a href={item.link}>{item.name}</a>
 						</li>
 					))}
