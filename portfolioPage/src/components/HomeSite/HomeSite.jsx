fdf0eaf5a2bf1c459f3b7e2e2d0caac6a1d4e7ca
Grzegorz <gkopacki92@gmail.com>
2024-05-07 23:37:08 +0200

    styled HomeSite component

--- portfolioPage/src/components/HomeSite/HomeSite.jsx
+++ portfolioPage/src/components/HomeSite/HomeSite.jsx
@@ -1,19 +1,22 @@
 import HERODARK from "../../assets/heroBlack.gif";
+import GITHUBICON from "../../assets/GithubIcon.svg";
+import LINKEDINICON from "../../assets/linkedin.svg";
+import styles from "./HomeSite.module.css";
 
 export function HomeSite() {
 	return (
-		<section id="home" class="home">
-			<img class="home-img" src={HERODARK} />
-			<div class="home-content">
+		<section id="home" className={styles.home}>
+			<img className={styles.homeImg} src={HERODARK} />
+			<div className={styles.homeContent}>
 				<h1>Grzegorz Kopacki</h1>
 				<span>Frontend Developer</span>
-				<div class="home-icons">
-					{/* <img src="./images/icons/linkedin.svg" alt="" />
-					<img src="./images/icons/GithubIcon.svg" alt="" /> */}
+				<div className={styles.homeIcons}>
+					<img src={GITHUBICON} />
+					<img src={LINKEDINICON} />
 				</div>
 			</div>
-			<button class="scroll-down-button">
-				<div class="scroll-down-circle"></div>
+			<button className={styles.scrollDownButton}>
+				<div className={styles.scrollDownCircle}></div>
 			</button>
 		</section>
 	);
