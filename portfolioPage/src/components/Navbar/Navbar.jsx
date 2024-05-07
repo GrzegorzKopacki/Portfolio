00e1b2d5c06bab870059237209f38d9aeb0e8fbc
Grzegorz <gkopacki92@gmail.com>
2024-05-08 01:04:20 +0200

    ,

--- portfolioPage/src/components/Navbar/Navbar.jsx
+++ portfolioPage/src/components/Navbar/Navbar.jsx
@@ -3,11 +3,9 @@ import { LANGUAGE } from "../../constants/language";
 import { BurgerButton } from "../BurgerButton/BurgerButton";
 import { SwitchMenu } from "../SwitchMenu/SwitchMenu";
 import styles from "./Navbar.module.css";
-import { NavLink } from "react-router-dom";
 
 export function Navbar({ language }) {
 	const selectedLanguage = LANGUAGE.find((lang) => lang.language === language);
-	const scroll = window.scrollY;
 
 	const [navMobileisActive, setNavMobileisActive] = useState(false);
 
@@ -15,20 +13,29 @@ export function Navbar({ language }) {
 		setNavMobileisActive((prevState) => !prevState);
 	}
 
+	function handleLinkClick(event) {
+		event.preventDefault();
+		if (window.innerWidth <= 1000) {
+			setNavMobileisActive(false);
+		}
+	}
+
 	return (
-		<div className={scroll > 0 ? styles.headerActive : styles.header}>
+		<div className={styles.header}>
 			<SwitchMenu />
 			<ul className={navMobileisActive ? styles.menuActive : styles.menu}>
 				{selectedLanguage &&
 					selectedLanguage.navbarItems.map((item, index) => (
 						<li className={styles.menuLink} key={index}>
-							<a href={item.link}>{item.name}</a>
+							<a href={item.link} onClick={handleLinkClick}>
+								{item.name}
+							</a>
 						</li>
 					))}
 			</ul>
 			<BurgerButton
 				isActive={navMobileisActive ? true : false}
-				onClick={() => handleMobileNav()}
+				onClick={handleMobileNav}
 			/>
 		</div>
 	);
