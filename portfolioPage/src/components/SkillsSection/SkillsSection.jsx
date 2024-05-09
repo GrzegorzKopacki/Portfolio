a29b95ba2e8ea6bebd4c19646301aadbf82d818b
Grzegorz <gkopacki92@gmail.com>
2024-05-09 23:34:40 +0200

    import tech icons

---
+++ portfolioPage/src/components/SkillsSection/SkillsSection.jsx
@@ -0,0 +1,17 @@
+import { FaHtml5 } from "react-icons/fa";
+import { FaCss3Alt } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io5";
+import { FaReact } from "react-icons/fa";
+import { SiTypescript } from "react-icons/si";
+import { FaNodeJs } from "react-icons/fa";
+import { SiTailwindcss } from "react-icons/si";
+import { TbBrandNextjs } from "react-icons/tb";
+import { SiVite } from "react-icons/si";
+import { FaSass } from "react-icons/fa";
+import { SiStyledcomponents } from "react-icons/si";
+import { FaGitAlt } from "react-icons/fa";
+import { TbBrandFirebase } from "react-icons/tb";
+
+export function SkillsSection() {
+	return <></>;
+}
