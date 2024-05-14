439c9bf750efef3d0bc301d3df98be7c736cad63
Grzegorz <gkopacki92@gmail.com>
2024-05-14 13:15:29 +0200

    added handle for sending email via contact section

--- portfolioPage/src/components/ContactForm/ContactForm.jsx
+++ portfolioPage/src/components/ContactForm/ContactForm.jsx
@@ -1,8 +1,29 @@
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
 import styles from "./ContactForm.module.css";
 export function ContactForm() {
+	const form = useRef();
+
+	const sendEmail = (e) => {
+		e.preventDefault();
+
+		emailjs
+			.sendForm("service_l4ncj4n", "template_dxuchjx", form.current, {
+				publicKey: "m1i-dZnjq6wOmWUqQ",
+			})
+			.then(
+				() => {
+					console.log("SUCCESS!");
+					e.target.reset();
+				},
+				(error) => {
+					console.log("FAILED...", error.text);
+				}
+			);
+	};
 	return (
 		<div className={styles.formContainer}>
-			<form className={styles.form}>
+			<form className={styles.form} ref={form} onSubmit={sendEmail}>
 				<label className={styles.label}>Name</label>
 				<input className={styles.input} type="text" name="user_name" />
 				<label className={styles.label}>Email</label>
